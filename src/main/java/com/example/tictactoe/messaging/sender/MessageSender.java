da3dd1791d8bdb7487207832ada789724b60f183
reywam <reywam@mail.ru>
2024-09-20 11:47:27 +0700

    Add PlayGameRequest and MessageSender

---
+++ src/main/java/com/example/tictactoe/messaging/sender/MessageSender.java
@@ -0,0 +1,18 @@
+package com.example.tictactoe.messaging.sender;
+
+import com.example.tictactoe.messaging.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MessageSender {
+    private final RabbitTemplate template;
+    private final FanoutExchange exchange;
+
+    public void send(PlayGameRequest event) {
+        this.template.convertAndSend(exchange.getName(), "", event);
+    }
+}
